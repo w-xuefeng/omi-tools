adc5d76e08dd58e8444cb3e5513734cc75523e1f
w-xuefeng <xuefeng@kcalb.wang>
2021-07-30 15:14:47 +0000

    fix: fixed createContext

--- src/omi-components/omiContext.ts
+++ src/omi-components/omiContext.ts
@@ -4,7 +4,8 @@ import reactive from './reactive'
 let CPCount = 0
 
 export interface ProviderProps<T> {
-  value: T
+  value: T,
+  setValue(value: T): void
 }
 
 export interface ConsumerProps { }
@@ -33,13 +34,19 @@ export interface IOmiContext<T> {
 
 export default function createContext<T>(defaultValue: T): IOmiContext<T> {
   CPCount++
-  const useContext = reactive<ProviderProps<T>>({ value: defaultValue })
+  const useContext = reactive<ProviderProps<T>>({
+    value: defaultValue,
+    setValue(value: T) {
+      this.value = value
+    }
+  })
   return {
     Provider: (() => {
       class Provider extends WeElement<ProviderProps<T>> {
-        render(props: Omi.RenderableProps<ProviderProps<T>>, store: IStore<T>) {
-          const { value } = useContext.apply(this) as ProviderProps<T>
-          store.context = props.value || value
+        render(props: Omi.RenderableProps<ProviderProps<T>>, store: IStore<ProviderProps<T>>) {
+          const context = useContext.apply(this) as ProviderProps<T>
+          context.setValue(props.value || context.value)
+          store.context = context
           return props.children
         }
       }
@@ -48,10 +55,10 @@ export default function createContext<T>(defaultValue: T): IOmiContext<T> {
     })(),
     Consumer: (() => {
       class Consumer extends WeElement<ConsumerProps> {
-        render(props: Omi.RenderableProps<ConsumerProps>, store: IStore<T>) {
+        render(props: Omi.RenderableProps<ConsumerProps>, store: IStore<ProviderProps<T>>) {
           const { children } = props
           return Array.isArray(children) && typeof children[0] === 'function'
-            ? children[0](store.context || defaultValue)
+            ? children[0](store.context)
             : children
         }
       }
