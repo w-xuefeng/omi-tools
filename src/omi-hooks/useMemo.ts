31028b33d003d87b97b9757d3bd77de89387dfad
w-xuefeng <xuefeng@kcalb.wang>
2021-08-15 11:27:48 +0000

    fix: fixed useMemo

--- src/omi-hooks/useMemo.ts
+++ src/omi-hooks/useMemo.ts
@@ -3,9 +3,21 @@ interface IMemoComputed {
   dependencies: any[]
 }
 const globalMemoryComputed = new Map<string, IMemoComputed>()
+const globalWeakMemoryComputed = new WeakMap<Function, IMemoComputed>()
 
 export default function useMemo<T>(callback: () => T, deps: any[]) {
   const key = callback.toString()
+
+  if (globalWeakMemoryComputed.has(callback)) {
+    const { data: prevData, dependencies: prevDeps = [] } = globalWeakMemoryComputed.get(callback) || {}
+    if (
+      prevDeps.length === deps.length
+      && prevDeps.every((dep: any, i: number) => dep === deps[i])
+    ) {
+      return prevData
+    }
+  }
+
   if (globalMemoryComputed.has(key)) {
     const { data: prevData, dependencies: prevDeps = [] } = globalMemoryComputed.get(key) || {}
     if (
@@ -15,7 +27,9 @@ export default function useMemo<T>(callback: () => T, deps: any[]) {
       return prevData
     }
   }
+
   const nextData = callback()
   globalMemoryComputed.set(key, { data: nextData, dependencies: deps })
+  globalWeakMemoryComputed.set(callback, { data: nextData, dependencies: deps })
   return nextData
 }
